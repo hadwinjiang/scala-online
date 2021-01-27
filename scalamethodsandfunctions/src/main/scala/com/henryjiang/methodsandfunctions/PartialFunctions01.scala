aab3e3e43e094fa4f6c631336b874cf95fccb2d1
henryjiang@mac <henryjiang@mac>
2021-01-27 09:35:23 +0800

    Paritial Function

---
+++ scalamethodsandfunctions/src/main/scala/com/henryjiang/methodsandfunctions/PartialFunctions01.scala
@@ -0,0 +1,22 @@
+package com.henryjiang.methodsandfunctions
+
+object PartialFunctions01 {
+
+  def main(args: Array[String]): Unit = {
+    //    val divide64By = (x: Int) => 64 / x
+    //    println(divide64By(8))
+    //    println(divide64By(0))
+
+    val divide64By = new PartialFunction[Int, Int] {
+      def apply(x: Int): Int = 64 / x
+
+      def isDefinedAt(x: Int): Boolean = x != 0
+    }
+
+    println("Defined at 11: " + divide64By.isDefinedAt(11))
+    println("Defined at 0: " + divide64By.isDefinedAt(0))
+
+    println("Dived 64 by 3: " + (if (divide64By.isDefinedAt(3)) divide64By(3)))
+  }
+
+}
---
+++ scalamethodsandfunctions/src/main/scala/com/henryjiang/methodsandfunctions/StockTicker.scala
@@ -0,0 +1,8 @@
+package com.henryjiang.methodsandfunctions
+
+case class StockTicker(date: String,
+                       open: Float,
+                       high: Float,
+                       low: Float,
+                       close: Float,
+                       ticker: String)
